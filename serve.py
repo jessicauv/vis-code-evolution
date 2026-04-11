5275801691493e41ee6136a5f3c518ea259f9030
Jessica <jessica.uviovo@yahoo.com>
2026-04-11 13:16:55 -0500

    Code cleanup - removing unnecessary files

--- serve.py
+++
@@ -1,5 +0,0 @@
-import os, http.server, socketserver
-os.chdir('/Users/jessicauviovo/Documents/vis-code-evolution/public')
-PORT = 3456
-with socketserver.TCPServer(("", PORT), http.server.SimpleHTTPRequestHandler) as httpd:
-    httpd.serve_forever()
